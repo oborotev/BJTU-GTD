f54102f836fef11a35d13eb5d1884a331affff9f
caille_j <caille_j@epitech.eu>
2016-03-08 04:46:11 +0000

    add of includes

---
+++ MeiYouCommunicationFramework/includes/TaiTaiP2PClient.h
@@ -0,0 +1,17 @@
+//
+// Created by storm on 08/03/16.
+//
+
+#ifndef MEIYOUCOMMUNICATION_TAITAIP2PCLIENT_H
+#define MEIYOUCOMMUNICATION_TAITAIP2PCLIENT_H
+
+# include <SFML/Network.hpp>
+
+class TaiTaiP2PClient {
+public:
+    TaiTaiP2PClient();
+    ~TaiTaiP2PClient() {};
+};
+
+
+#endif //MEIYOUCOMMUNICATION_TAITAIP2PCLIENT_H

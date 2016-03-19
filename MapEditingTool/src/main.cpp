962aad8b5e5ed5eeed7861b80b9ad18b69d7063e
guigur <guigur13@gmail.com>
2016-03-19 17:54:26 +0800

    addnewsprinte not working

--- MapEditingTool/src/main.cpp
+++ MapEditingTool/src/main.cpp
@@ -2,9 +2,16 @@
 
 int main()
 {
+ /*
     MapEditor mapEditor;
 
     mapEditor.init();
     mapEditor.start();
+    */
+
+    MediaHandler lojl;
+
+    lojl.addNewTexture("/home/margoul_1/Images/44444.png", "test");
+    lojl.addNewSprite("jeej");
     return 0;
 }
--- MapEditingTool/src/mediaHandler.cpp
+++ MapEditingTool/src/mediaHandler.cpp
@@ -19,7 +19,27 @@ const int   MediaHandler::addNewTexture(const std::string &path, const std::stri
 
 const int   MediaHandler::addNewSprite(const std::string &textureName)
 {
+    std::cout << _textures.size() << std::endl;
+  /*  for(unsigned int i = 0; i < _textures.size(); i++)
+    {*/
+    std::cout << this->_textures[0].second << std::endl;
 
+    if (this->_textures[0].second == "test")
+            std::cout << "ta" << std::endl;
+   // }
+/*
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+
+    if (!texture.loadFromFile(path))
+    {
+        std::cout << "Problem while loading the texture";
+        return (1);
+    }
+    this->_textures.push_back(std::make_pair(texture, name));
+    //std::cout << this->_textures[0].second << std::endl;
+    return (0);
+*/
 }
 
 sf::Texture MediaHandler::getTexture(const std::string &name)

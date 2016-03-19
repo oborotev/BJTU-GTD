d806ee2b00f212ce26c3891cb5a890ddb557ae5c
guigur <guigur13@gmail.com>
2016-03-19 16:03:24 +0800

    le c++ c'est du c++

--- MapEditingTool/src/main.cpp
+++ MapEditingTool/src/main.cpp
@@ -2,8 +2,13 @@
 
 int main()
 {
+    /*
     GraphicHandler graphicHandler("MapEditor");
 
-    graphicHandler.init();
+    graphicHandler.init();*/
+    MediaHandler  loul;
+    std::string test = "lol";
+    std::string test2 = "jeej";
+    loul.loadNewTexture( test , test2);
     return 0;
 }
--- MapEditingTool/src/mediaHandler.cpp
+++ MapEditingTool/src/mediaHandler.cpp
@@ -3,3 +3,11 @@
 //
 
 # include "mediaHandler.h"
+
+const int   MediaHandler::loadNewTexture(const std::string &path, const std::string &name)
+{
+    std::string test = "loul";
+    std::string test2 = "lolll";
+    this->_textures.push_back(std::make_pair( test, test2));
+
+}
